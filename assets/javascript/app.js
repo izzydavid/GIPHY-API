c788d6e9f0b99c59ca5e6521096ece6a65f40c1c
IzzyD <davidirizarry@Davids-MacBook-Pro.local>
2019-06-19 01:39:16 -0400

    Second Version of GIPHY-API Assignment

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+var topics = ["cowboy bepop", "trigun", "my neighbor totoro", "death note"]; 
+
+$("button").on("click", function() {
+    // Grabbing and storing the data-anime property value from the button
+    var anime = $(this).attr("data-name");
+
+    // Constructing a queryURL using the anime name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      anime + "&api_key=Qxpp4x5d7fMc17qfyggEeDXHcJFmzIWO&limit=10";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+
+          // Creating and storing a div tag
+          var animeDiv = $("<div>");
+
+          // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + results[i].rating);
+
+          // Creating and storing an image tag
+          var animeImage = $("<img>");
+          // Setting the src attribute of the image to a property pulled off the result item
+          animeImage.attr("src", results[i].images.fixed_height.url);
+
+          // Appending the paragraph and image tag to the animeDiv
+          animeDiv.append(p);
+          animeDiv.append(animeImage);
+          
+          $(".gif").on("click", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+          // Prependng the animeDiv to the HTML page in the "#gifs-appear-here" div
+          $("#gifs-appear-here").prepend(animeDiv);
+        }
+      });
+      
+  });
+
