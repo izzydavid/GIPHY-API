bf3c992b6bef47ad3887e94d1a74413073c5dbab
Ubernator <43094772+izzydavid@users.noreply.github.com>
2019-10-18 22:40:12 -0400

    styled it a bit better than it was last time, I like it more now, just need to fix the delete function and store more than one gify button at a time and should be good

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,90 @@
-var topics = ["cowboy bepop", "trigun", "my neighbor totoro", "death note"];
+function intiialButtons() {
+  $("#buttons").empty();
+  topics.forEach(function (anime, index) {
+  var button = $("#buttons");
+  button.addClass('<waves-effect btn-small>').text(topics[index]).attr("data-anime", topics[index]); 
+  $("#buttons").append(button);
+  }); 
+  M.AutoInit();
+  $('.tooltipped').tooltip();
 
-$(".nav-item").on("click", function intiialButtons() {
-  // Grabbing and storing the data-anime property value from the button
-  var anime = $(this).attr("data-name");
+}
 
-  // Constructing a queryURL using the anime name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    anime + "&api_key=Qxpp4x5d7fMc17qfyggEeDXHcJFmzIWO&limit=10";
-
-  // Performing an AJAX request with the queryURL
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    // After data comes back from the request
-    .then(function (response) {
-      console.log(queryURL);
-      console.log(response);
+function addAnimeButton() {
+  $("#addAnime").on("click", function (event) {
+    event.preventDefault();
+    var newAnimeButton = $("#animeInput").val().trim();
+    if (newAnimeButton === "") {
+      return false;
+    } else {
+      topics.push(newAnimeButton);
+      intiialButtons();
+    }
+  });
+  M.AutoInit();
+}
 
 
-      
-      //The beginning of the pause and play of Gifs, only the second results are working instead of all gifs. 
-      for (var i = 0; i < 10; i++) {
-        console.log(response.data[i].images.original_still);
+$("#delete").on("click", "#delete", function () {
+  intiialButtons();
+  M.AutoInit();
+})
 
-        $('.grid-item').prepend(`<img class='anime gif' src='${response.data[i].images.original_still.url}
-          'data-still='${response.data[i].images.original_still.url}
-          'data-animate='${response.data[i].images.original.url}
-          'data-state=''><span class='rate'>${response.data[i].rating}</span>`);
-      
-      $(".gif").on("click", function () {
-        var state = $(this).attr("data-state");
-        // console.log(state);
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
+
+
+          var topics = ["cowboy bepop", "trigun", "totoro", "death note"];
+          $("#buttons").on("click", function () {
+            //Grabbing and storing the data-anime property value from the button
+            var anime = $(this).attr("data-anime"); 
+            //Constructing a queryURL using the anime name
+            var queryURL =
+              "https://api.giphy.com/v1/gifs/search?q=" +
+              anime +
+              "&api_key=Qxpp4x5d7fMc17qfyggEeDXHcJFmzIWO&limit=36"; 
+              //Performing an AJAX request with the queryURL
+            $.ajax({
+              url: queryURL,
+              method: "GET"
+            }) 
+            //After data comes back from the request
+              .then(function(response) {
+                console.log(queryURL);
+                console.log(response); 
+                //Start of For Loop for images and pushing the Gify images into the
+                //Materialize framework/cards and establishing the two different states of the
+                //Images "Still" and "Animated" when someone enters and leaves the element//
+                for (var i = 0; i < 36; i++) {
+                  console.log(response.data[i].images.original.url);
+                $(".carousel").prepend(`<div class="carousel-item"> 
+                <img class="card-image hoverable"
+                src='${response.data[i].images.fixed_height_still.url}',  
+                data-still='${response.data[i].images.fixed_height_still.url}' data-animate='${response.data[i].images.fixed_height.url}' data-state='still'> 
+                <div class="card-action"> <a href="${response.data[i].url}" target="_blank">Click Here</a>
+          </div>
+      </div>`);
+            }
+          }
+        );
+
+
+        $(".card-image").hover(function() {
+          //The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+          var state = $(this).attr("data-state"); // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+          //Then, set the image's data-state to animate
+          //Else set src to the data-still value
+  
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        }); //End of For Loop for images and pushing the Gify images into the materialize framework/cards and establishing the two different states of the images "Still" and "Animated" when someone enters and leaves the element//
       });
-      } 
-      //The end of the pause and play of gifs
+            
 
 
-        // Prependng the animeDiv to the HTML page in the "#gifs-appear-here" div
-      }); 
-});
+          addAnimeButton(); 
+          intiialButtons();
+          M.AutoInit();
