9ce4f6a94d199b789ea72cb0bed311bcd3104fac
IzzyDavid <david.irizarry86@yahoo.com>
2019-10-19 13:17:59 -0400

    Made updates, just need to finish the download and more than one button. I receive the length of the buttons when I use $<#buttons2> but it doesn make the ajax call. Updated the CORS and fetch instead of get due to errors.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,84 @@
-function intiialButtons() {
-  $("#buttons").empty();
-  topics.forEach(function (anime, index) {
-  var button = $("#buttons");
-  button.addClass('<waves-effect btn-small>').text(topics[index]).attr("data-anime", topics[index]); 
-  $("#buttons").append(button);
-  }); 
-  M.AutoInit();
-  $('.tooltipped').tooltip();
+var topics = ["cowboy bepop", "trigun", "my neighbor totoro", "death note"];
 
-}
+$(".nav-item").on("click", function intiialButtons() {
+  // Grabbing and storing the data-anime property value from the button
+  var anime = $(this).attr("data-name");
 
-function addAnimeButton() {
-  $("#addAnime").on("click", function (event) {
-    event.preventDefault();
-    var newAnimeButton = $("#animeInput").val().trim();
-    if (newAnimeButton === "") {
-      return false;
-    } else {
-      topics.push(newAnimeButton);
-      intiialButtons();
-    }
-  });
-  M.AutoInit();
-}
+  // Constructing a queryURL using the anime name
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    anime + "&api_key=Qxpp4x5d7fMc17qfyggEeDXHcJFmzIWO&limit=12";
 
-
-$("#delete").on("click", "#delete", function () {
-  intiialButtons();
-  M.AutoInit();
-})
+  // Performing an AJAX request with the queryURL
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    // After data comes back from the request
+    .then(function (response) {
+      console.log(queryURL);
+      console.log(response);
 
 
 
-          var topics = ["cowboy bepop", "trigun", "totoro", "death note"];
-          $("#buttons").on("click", function () {
-            //Grabbing and storing the data-anime property value from the button
-            var anime = $(this).attr("data-anime"); 
-            //Constructing a queryURL using the anime name
-            var queryURL =
-              "https://api.giphy.com/v1/gifs/search?q=" +
-              anime +
-              "&api_key=Qxpp4x5d7fMc17qfyggEeDXHcJFmzIWO&limit=36"; 
-              //Performing an AJAX request with the queryURL
-            $.ajax({
-              url: queryURL,
-              method: "GET"
-            }) 
-            //After data comes back from the request
-              .then(function(response) {
-                console.log(queryURL);
-                console.log(response); 
-                //Start of For Loop for images and pushing the Gify images into the
-                //Materialize framework/cards and establishing the two different states of the
-                //Images "Still" and "Animated" when someone enters and leaves the element//
-                for (var i = 0; i < 36; i++) {
-                  console.log(response.data[i].images.original.url);
-                $(".carousel").prepend(`<div class="carousel-item"> 
-                <img class="card-image hoverable"
-                src='${response.data[i].images.fixed_height_still.url}',  
-                data-still='${response.data[i].images.fixed_height_still.url}' data-animate='${response.data[i].images.fixed_height.url}' data-state='still'> 
-                <div class="card-action"> <a href="${response.data[i].url}" target="_blank">Click Here</a>
-          </div>
-      </div>`);
-            }
-          }
-        );
+      //The beginning of the pause and play of Gifs, only the first results are working instead of all gifs. I can play and pause every other gif.  
+      for (var i = 0; i < 12; i++) {
+        console.log(response.data[i].images.original_still.url);
 
+        $('.grid-container').prepend(`
+        <div class="col s12 m7"> </div> 
+          <div class="card"> 
+            <img class="card-image" src = '${response.data[i].images.original_still.url}'data-still='${response.data[i].images.original_still.url}
+            'data-animate='${response.data[i].images.original.url}
+            'data-state='${response.data[i].images.original_still.url}'
+            <span class="card-title">${response.data[i].title}</span>
+            <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+         
+              </div> 
+           </div>
+        </div>
+        
+        `)
 
-        $(".card-image").hover(function() {
-          //The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-          var state = $(this).attr("data-state"); // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-          //Then, set the image's data-state to animate
-          //Else set src to the data-still value
-  
+        $(".card-image").on("click", function () {
+          var state = $(this).attr("data-state");
+          // console.log(state);
           if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
           } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
+
           }
-        }); //End of For Loop for images and pushing the Gify images into the materialize framework/cards and establishing the two different states of the images "Still" and "Animated" when someone enters and leaves the element//
-      });
-            
+        });
+      }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
 
 
-          addAnimeButton(); 
-          intiialButtons();
-          M.AutoInit();
+// Or with jQuery
+
+$(document).ready(function () {
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": 'https://placehold.it/250x250'
+    },
+  });
+});
+
+$(".btn-large").on("click", function
+
+
+
+
+
+
+
+)
